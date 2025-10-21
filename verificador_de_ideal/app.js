b5d5d750c6cf180a1d07b8a646b87ad58cd363bd
JLuisusr <luis.work0124@gmail.com>
2025-10-21 08:03:01 -0300

    up

--- verificador_de_ideal/app.js
+++ verificador_de_ideal/app.js
@@ -7,11 +7,46 @@ function verificar(){
     let idade = data - anoNascimento;
     let sexo = document.getElementsByName('genero');
     let txtResultado= document.getElementById('resultado');
-        txtResultado.innerHTML = `Idade calculad: ${idade} ${sexo[0].value}`;
-    if ( 0<= idade <=10){
+    let img = document.createElement('img')
+        
+
+
+    if ( sexo[0].checked){
+        if (idade <=10){
+            //criança
+        }
+        else if (idade <=20){
+            //jovem
+        }
+        else if (idade <=50){
+            //adulto
+        }
+        else{
+            //idoso
+        }
+        
 
         
     }
+    else{
+        if(idade <=10){
+            //criança
+            img.setAttribute('src','./mulher_idosa.jpg')              
+        }
+        else if (idade <=20){
+            //jovem
+        }
+        else if (idade <=50){
+            //adulto
+        }
+        else{
+            //idoso
+        }
+        
+    }
     
+    txtResultado.innerHTML = `Idade calculad: ${idade} ${sexo[0].value}`;
+    txtResultado.appendChild(img)
+
 
 }
