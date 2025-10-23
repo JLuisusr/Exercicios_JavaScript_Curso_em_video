02ec709b21860b0e4a102b548a7762028ede39e6
JLuisusr <luis.work0124@gmail.com>
2025-10-23 14:38:22 -0300

    up

--- vamos_contar/app.js
+++ vamos_contar/app.js
@@ -1,11 +1,11 @@
 function contagem(inicio,fim,passo,textoContagem){
     for(var i = inicio; i <=fim; i = i + passo){
         
-        textoContagem.innerHTML += `&#x1F919&#x1F3FC ${i} `
+        textoContagem.innerHTML += `&#x1F449&#x1F3FC ${i} `
              
 
     }
-    return i 
+    textoContagem.innerHTML += `&#x1F3C1`
 }
  
 function conta(){
@@ -14,6 +14,7 @@ function conta(){
         texto.id = 'textoConte';
     document.getElementById('resultado').appendChild(texto);
     var textoContagem = document.getElementById('textoConte')
+    textoContagem.innerHTML = ''
     var inicio = parseInt(document.getElementById('inicio').value)
     var fim = parseInt(document.getElementById('fim').value)
     var passo = parseInt(document.getElementById('passo').value)
