9c1b001281ed779dad85a4c417fb3f6c99c39456
Luis <jl.studiositas@gmail.com>
2025-10-24 13:44:24 -0300

    up

---
+++ tabuada/app.js
@@ -0,0 +1,12 @@
+function tabuada(){
+    let numero = parseInt(document.getElementById('number').value)
+    if( isNaN(numero)){
+        alert('Entrada invalida')
+    }
+
+    else{
+        
+    }
+
+
+}
