6844d7c1766e904f7c117b0c1252f9b2639adaa9
Luis <jl.studiositas@gmail.com>
2025-10-26 19:20:15 -0300

    up

--- tabuada/app.js
+++ tabuada/app.js
@@ -1,3 +1,4 @@
+
 function tabuada(){
     let numero = parseInt(document.getElementById('number').value)
     if( isNaN(numero)){
@@ -5,7 +6,19 @@ function tabuada(){
     }
 
     else{
-        
+        var tab = document.getElementById('tabuada')
+        tab.innerHTML = ''
+        tab.setAttribute('size','10')
+
+        for(let i = 0; i < 10; i++){
+            let elemento= document.createElement('option')
+
+            elemento.text = `${numero} x ${i} = ${numero * i}`
+            elemento.value = `tab${i}`
+            tab.appendChild(elemento)
+            
+
+        }
     }
 
 
